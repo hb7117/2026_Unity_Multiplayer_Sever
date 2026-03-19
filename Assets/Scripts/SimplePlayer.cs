caa9a8483d575fd7de2c1e527154e44e16eece5b
hb7117 <bin20050711@gmail.com>
2026-03-19 18:16:15 +0900

    [Feat] host client 프리팹 생성, 이동, 회전 동기화

---
+++ Assets/Scripts/SimplePlayer.cs
@@ -0,0 +1,20 @@
+using Fusion;
+using UnityEngine;
+
+public class SimplePlayer : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 5f;
+
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
